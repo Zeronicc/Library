67b9eecdc538b0fa0f3edd1d292504be17e80b8f
Zeronicc <zeronic4465@gmail.com>
2022-12-18 04:48:12 -0600

    Updated card for better appeal along with background

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const pagesInput = document.querySelector("#pages-box");
 const readInput = document.querySelector("#checkbox");
 const libraryContainer = document.querySelector(".display-books")
 
+
 addBtn.addEventListener('click', (e) =>{
     e.preventDefault();
     addBookToLibrary();
@@ -27,36 +28,65 @@ function addBookToLibrary() {
     if(title == "" || author == "" || pages == "") return;
     if(myLibrary.some(book => book.title === title)) return;
     let newBook = new Book(title, author, pages, read);
-    
     createBookCard(newBook);
-    myLibrary.push(newBook);
-    console.log(myLibrary);
-    
+    myLibrary.push(newBook);    
 }
 
 function createBookCard(book) {
+    const cardContainer = document.createElement('div')
     const title = document.createElement('p')
     const author = document.createElement('p')
     const pages = document.createElement('p')
-    const checkbox = document.createElement('input')
-
-    checkbox.type = "checkbox"
-    checkbox.classList.add("library-checkbox")
-
+    let readBtn = document.createElement('button')
+    const deleteBtn = document.createElement('button')
+    
+    cardContainer.classList.add('card-container')
+    deleteBtn.classList.add('delete-icon')
+    readBtn.classList.add('read-btn')
 
     title.textContent = `"${book.title}"`
     author.textContent = `${book.author}`
-    pages.textContent = `${book.pages}`
+    pages.textContent = `${book.pages} Pages`
+    readBtn.textContent = "Read"
+
+    
 
     if(book.read){
-        checkbox.checked = true
-    }else{checkbox.checked = false}
+        readBtn.classList.add('read-green')
+    }else{readBtn.classList.add('read-red')}
+    
+    book1 = book.title
 
-    libraryContainer.appendChild(title);
-    libraryContainer.appendChild(title);
-    libraryContainer.appendChild(author);
-    libraryContainer.appendChild(pages);
-    libraryContainer.appendChild(checkbox);
+    if(book1.length > 14){
+        title.style.fontSize = '1.3em'
+    }else if(book1.length > 11){
+        title.style.fontSize = '1.4em'
+    }
+
+    readBtn.addEventListener('click', e => {
+        if(book.read){
+            readBtn.classList.remove('read-green')
+            readBtn.classList.add('read-red')
+            book.read = false
+            console.log(book)
+        }else{
+            readBtn.classList.remove('read-red')
+            readBtn.classList.add('read-green')
+            book.read = true
+            console.log(book)
+        }
+    })
+
+    deleteBtn.addEventListener('click', e => {
+        cardContainer.remove()
+    })
+
+    libraryContainer.appendChild(cardContainer);
+    cardContainer.appendChild(title);
+    cardContainer.appendChild(author);
+    cardContainer.appendChild(pages);
+    cardContainer.appendChild(readBtn);
+    cardContainer.appendChild(deleteBtn)
 }
 
 
